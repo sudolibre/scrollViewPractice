1ce2d55ddb1d23d19937d73b676c9237fcfc918e
sudolibre <optout.breaker@gmail.com>
2017-02-13 21:18:14 -0500

    testing scrolling content bigger than the screen

--- ScrollPractice/ViewController.swift
+++ ScrollPractice/ViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var outterView: UIView!
+    var scrollView: UIScrollView {
+        return view as! UIScrollView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        scrollView.contentSize = outterView.bounds.size
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
